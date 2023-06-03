96414ac8be8e166aa784fdc397d67c75d91d1e43
Dadang <muhdadangs@gmail.com>
2023-06-04 04:52:36 +0700

    fix background image of camera item

--- lib/widgets/camera_item.dart
+++ lib/widgets/camera_item.dart
@@ -31,10 +31,6 @@ class CameraItem extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(8, 30, 8, 0),
         margin: const EdgeInsets.all(8.0),
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background_image.png'),
-            fit: BoxFit.cover,
-          ),
           color: Colors.white,
           borderRadius: BorderRadius.circular(20),
           boxShadow: [
@@ -46,74 +42,87 @@ class CameraItem extends StatelessWidget {
             ),
           ],
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: [
             Align(
-              alignment: Alignment.center,
-              child: Container(
-                width: 110,
-                height: 110,
-                margin: EdgeInsets.only(bottom: 20),
-                child: Image.network(
-                  documentSnapshot['gambar'],
-                  width: size.width * 0.24,
-                  loadingBuilder: (context, child, event) => event == null
-                      ? child
-                      : Center(
-                          child: CircularProgressIndicator(
-                            value: event.cumulativeBytesLoaded /
-                                (event.expectedTotalBytes ?? 1),
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                  errorBuilder: (context, url, error) => Icon(
-                    Icons.broken_image_outlined,
-                    color: Colors.grey,
-                    size: size.width * 0.24,
+              alignment: Alignment.topCenter,
+              child: Image.asset(
+                "assets/images/background_image.png",
+                fit: BoxFit.fill,
+                width: 130,
+                height: 125,
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    width: 110,
+                    height: 110,
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Image.network(
+                      documentSnapshot['gambar'],
+                      width: size.width * 0.24,
+                      loadingBuilder: (context, child, event) => event == null
+                          ? child
+                          : Center(
+                              child: CircularProgressIndicator(
+                                value: event.cumulativeBytesLoaded /
+                                    (event.expectedTotalBytes ?? 1),
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                      errorBuilder: (context, url, error) => Icon(
+                        Icons.broken_image_outlined,
+                        color: Colors.grey,
+                        size: size.width * 0.24,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              documentSnapshot['namaProduk'],
-              textAlign: TextAlign.left,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                    fontSize: 10,
-                    color: Color(0xFF262626),
-                    fontFamily: 'FontPoppins',
-                    fontWeight: FontWeight.w400,
+                SizedBox(height: 20.0),
+                Text(
+                  documentSnapshot['namaProduk'],
+                  textAlign: TextAlign.left,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                        fontSize: 10,
+                        color: Color(0xFF262626),
+                        fontFamily: 'FontPoppins',
+                        fontWeight: FontWeight.w400,
+                      ),
+                ),
+                SizedBox(height: 4.0),
+                if (q != null)
+                  Text(
+                    "Nilai: ${q!.toStringAsFixed(3)}",
+                    textAlign: TextAlign.left,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                          fontSize: 10,
+                          color: Color(0xFF262626),
+                          fontFamily: 'FontPoppins',
+                          fontWeight: FontWeight.w600,
+                        ),
                   ),
-            ),
-            SizedBox(height: 4.0),
-            if (q != null)
-              Text(
-                "Nilai: ${q!.toStringAsFixed(3)}",
-                textAlign: TextAlign.left,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                      fontSize: 10,
-                      color: Color(0xFF262626),
-                      fontFamily: 'FontPoppins',
-                      fontWeight: FontWeight.w600,
-                    ),
-              ),
-            SizedBox(height: 4.0),
-            Text(
-              "Rp. ${documentSnapshot['harga']}",
-              textAlign: TextAlign.left,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                    fontSize: 10,
-                    color: Color(0xFF262626),
-                    fontFamily: 'FontPoppins',
-                    fontWeight: FontWeight.w600,
-                  ),
+                SizedBox(height: 4.0),
+                Text(
+                  "Rp. ${documentSnapshot['harga']}",
+                  textAlign: TextAlign.left,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                        fontSize: 10,
+                        color: Color(0xFF262626),
+                        fontFamily: 'FontPoppins',
+                        fontWeight: FontWeight.w600,
+                      ),
+                ),
+              ],
             ),
           ],
         ),
